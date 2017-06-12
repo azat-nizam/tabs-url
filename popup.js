a151c691f20b3f79306877752be62ae54455a5df
Azat Nizameev <azat.nizam@gmail.com>
2017-06-12 21:51:36 +0300

    fix url proccessing

--- popup.js
+++ popup.js
@@ -10,8 +10,9 @@ function openField() {
 	document.getElementById('go-but').addEventListener('click', function() {
 		strLinks = document.getElementById('links').value;
 		arStr = strLinks.split('\n');
-		
+
 		for (var i = 0; i < arStr.length; i++) {
+			arStr[i] = arStr[i].replace(/\!/g, '');
 			chrome.tabs.create({url:arStr[i]});
 		}
 	});
@@ -19,7 +20,7 @@ function openField() {
 
 document.addEventListener('DOMContentLoaded', function() {
 	document.getElementById('open-but').addEventListener('click', openField);
-	
+
 	chrome.tabs.query({currentWindow: true}, function(tabs) {
 		// A tab is a plain object that provides information about the tab.
 		// See https://developer.chrome.com/extensions/tabs#type-Tab
